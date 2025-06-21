9a4e9160ac9995cc4994ca25ca52ca3ad42648d7
Dimswati <dimswati@proton.me>
2025-06-21 04:02:09 -0700

    update telegram button

--- app/(auth)/sign-up/connect-telegram/page.tsx
+++ app/(auth)/sign-up/connect-telegram/page.tsx
@@ -25,10 +25,11 @@ const ConnectTelegram = () => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?7';
     script.setAttribute('data-telegram-login', 'FundedTradeBot');
-    script.setAttribute('data-size', 'large');
+    script.setAttribute('data-size', 'medium');
     script.setAttribute('data-userpic', 'false');
     script.setAttribute('data-request-access', 'write');
     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.setAttribute('data-radius', '5')
     script.async = true;
 
     window.onTelegramAuth = function (user: any) {
@@ -50,7 +51,7 @@ const ConnectTelegram = () => {
       <h2 className='text-center text-2xl font-bold mb-2'>Connect Telegram</h2>
       <p className='text-center text-base mb-6'>All Communications on funding trade requests will go here</p>
       <div className="flex items-center justify-center">
-        <a className={cn(buttonVariants())} id="telegram-login-btn">connect telegram</a>
+        <div id="telegram-login-btn"></div>
       </div>
     </div>
   )
